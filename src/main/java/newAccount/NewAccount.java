9da17706c4f781786e765bf574c0814eb9e330db
zotyo911 <sooszoltan79@gmail.com>
2021-07-31 09:41:38 +0200

    try again

--- src/main/java/newAccount/NewAccount.java
+++ src/main/java/newAccount/NewAccount.java
@@ -1,7 +1,5 @@
 package newAccount;
 
-import newAccount.NewAccountElements;
-
 public class NewAccount {
     public static void createANewAccount(){
         NewAccountElements.createANewAccountButton.click();
